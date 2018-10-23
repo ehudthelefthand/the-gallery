c3831f124da130bc14bb6e64344f17ebe36e8a1c
ehudthelefthand <peerawat.pm@gmail.com>
2018-10-23 14:16:30 +0700

    added controllers

--- server.js
+++ server.js
@@ -4,39 +4,12 @@ const usersC = require('./controllers/users')
 const backdoorC = require('./controllers/backdoor')
 const port = 3000
 
-const mw1 = (req, res, next) => {
-  console.log('mw1')
-  next()
-}
-
-const mw2 = (req, res, next) => {
-  console.log('mw2')
-  next()
-}
-
-const mwafter = (req, res, next) => {
-  console.log('mw after')
-}
-
-const finalerror = (err, req, res, next) => {
-  console.log(err)
-}
-
-const mwall = (req, res, next) => {
-  console.log('mw all')
-  res.send('ok again')
-  next()
-}
-
-app.use(mwall)
-app.use(finalerror)
-
 // System routes
 app.get('/', mw2, mw1, backdoorC.check, mwafter)
 
 // User routes
-// app.post('/users', usersC.register)
-// app.post('/login', usersC.login)
-// app.post('/logout', usersC.logout)
+app.post('/users', usersC.register)
+app.post('/login', usersC.login)
+app.post('/logout', usersC.logout)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
